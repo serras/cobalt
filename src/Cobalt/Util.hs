144587a86960e67f2205f6c81b4b911158f97101
Alejandro Serrano <trupill@gmail.com>
2014-10-22 09:41:48 +0200

    Forgot Utils module

---
+++ src/Cobalt/Util.hs
@@ -0,0 +1,27 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+module Cobalt.Util where
+
+import Control.Monad.Except
+import Data.List (intercalate)
+import Data.List.Split
+import Unbound.LocallyNameless
+
+instance Fresh m => Fresh (ExceptT e m) where
+  fresh = lift . fresh
+
+replace :: Eq a => [a] -> [a] -> [a] -> [a]
+replace old new l = intercalate new . splitOn old $ l
+
+withGreek :: String -> String
+withGreek = replace "alpha" "α"
+            . replace "beta" "β"
+            . replace "tau" "τ"
+            . replace "->" "→"
+            . replace "=>" "⟹"
+
+showWithGreek :: Show a => a -> String
+showWithGreek = withGreek . show
+
+doParens :: String -> String
+doParens s | ' ' `elem` s = '(' : s ++ ")"
+           | otherwise    = s
